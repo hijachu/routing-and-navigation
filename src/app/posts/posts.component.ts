30dabb851a7f910a1538cc4b2b9a08b85d1c7e61
YuChih Hu <yuchih@honeywld.com.tw>
2020-02-04 12:11:29 +0800

    Delegate a post service

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from './../services/post.service';
 
 @Component({
   selector: 'app-posts',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
+  posts: any[];
 
-  constructor() { }
+  constructor(private service: PostService) {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.service.getPosts()
+      .subscribe(response => {
+        this.posts = response;
+      });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+    input.value = '';
+
+    this.service.createPost(post)
+      .subscribe((response: any) => {
+        post['id'] = response.id;
+        this.posts.splice(0, 0, post);
+      });
+  }
+
+  updatePost(post) {
+    this.service.updatePost(post)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
+  deletePost(post) {
+    this.service.deletePost(post.id)
+      .subscribe(response => {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+      });
+  }
 }
