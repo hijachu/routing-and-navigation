33b3d312e321c7314dd070c8630938a4a4c2e6fb
YuChih Hu <yuchih@honeywld.com.tw>
2020-02-07 15:08:00 +0800

    Query parameters

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,6 @@
-import { GithubFollowersService } from './../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GithubFollowersService } from './../services/github-followers.service';
 
 @Component({
   selector: 'github-followers',
@@ -9,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService) { }
 
   ngOnInit() {
+    // let id = this.route.snapshot.paramMap['id'];
+    this.route.paramMap.subscribe(params => {
+
+    });
+
+    // let page = this.route.snapshot.queryParamMap['page'];
+    this.route.queryParamMap.subscribe(params => {
+      
+    });
+
     this.service.getAll()
       .subscribe(followers => this.followers = followers);
   }
