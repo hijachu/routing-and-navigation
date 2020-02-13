666a92104db839b357439338b5bfaf3ad534c205
YuChih Hu <hijac.hu@gmail.com>
2020-02-14 01:35:04 +0800

    programmatic navigation Implement submit button to return followers list page

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
@@ -8,16 +8,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
-    console.log(this.route.snapshot.paramMap['id']);
+    // console.log(this.route.snapshot.paramMap['id']);
 
-    // this.route.paramMap
-    //   .subscribe(params => {
-    //     let id = +params.get('id');
-    //     console.log(id);
-    //   });
+    this.route.paramMap
+      .subscribe(params => {
+        let id = +params.get('id');
+        console.log(id);
+      });
   }
 
+  submit() {
+    this.router.navigate(['/followers'], {
+      queryParams: { page: 1, order: 'newest' }
+    });
+  }
 }
